5b023b1429c1007dac4088bb5ede39b2987ead47
GemmaTuron <gemma@ersilia.io>
2024-04-08 12:09:04 +0200

    upgrade version eosce

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="lazyqsar",
-    version="0.2",
+    version="0.4",
     author="Miquel Duran-Frigola",
     author_email="miquel@ersilia.io",
     url="https://github.com/ersilia-os/lazy-qsar",
