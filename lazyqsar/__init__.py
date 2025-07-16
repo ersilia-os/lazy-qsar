4b1f0896cc4c252507d4af9ab86eec2d81585e17
Arnau <80755454+arnaucoma24@users.noreply.github.com>
2025-07-16 11:23:41 +0200

    Update __init__.py

--- lazyqsar/__init__.py
+++ lazyqsar/__init__.py
@@ -1,7 +1,7 @@
 try:
     from .qsar import LazyBinaryQSAR
 except Exception as e: 
-    print("You are not using the full version of lazy-qsar which has descriptord pipeline!")
+    print("You are not using the full version of lazy-qsar which has descriptors pipeline!")
     print(e)
     pass
 from .qsar_descriptor_free import LazyBinaryQSAR as DescriptorFreeLazyQSAR
