0cf50a158bae19f8e4ff296d237bbe7273de26a1
Abellegese <abellegese5@gmail.com>
2025-08-12 18:24:05 +0300

    chore(logger): standardizing logger style in lq modules

--- lazyqsar/utils/logging.py
+++ lazyqsar/utils/logging.py
@@ -1,87 +1,68 @@
-import os
-from loguru import logger
+import logging
+import optuna
 from rich.logging import RichHandler
-import logging, optuna
-from loguru import logger
+from loguru import logger as _loguru 
 
 class InterceptHandler(logging.Handler):
     def emit(self, record):
         try:
-            level = logger.level(record.levelname).name
+            level = _loguru.level(record.levelname).name
         except ValueError:
             level = record.levelno
-        logger.opt(depth=6, exception=record.exc_info).log(level, record.getMessage())
+        _loguru.opt(depth=6, exception=record.exc_info).log(level, record.getMessage())
 
 def setup_optuna_logging(level=logging.INFO):
-    optuna.logging.disable_default_handler()  
-    optuna.logging.enable_propagation()       
+    optuna.logging.disable_default_handler()
+    optuna.logging.enable_propagation()
     lg = logging.getLogger("optuna")
-    lg.handlers = [InterceptHandler()]        
+    lg.handlers = [InterceptHandler()]
     lg.setLevel(level)
 
-setup_optuna_logging(level=logging.INFO)
+setup_optuna_logging(logging.INFO)
 
 ROTATION = "10 MB"
 
-logger.remove()
-
-logger.level("DEBUG", color="<cyan><bold>")
-logger.level("INFO", color="<blue><bold>")
-logger.level("WARNING", color="<white><bold><bg yellow>")
-logger.level("ERROR", color="<white><bold><bg red>")
-logger.level("CRITICAL", color="<white><bold><bg red>")
-logger.level("SUCCESS", color="<black><bold><bg green>")
-
+_loguru.remove()
+_loguru.level("DEBUG", color="<cyan><bold>")
+_loguru.level("INFO", color="<blue><bold>")
+_loguru.level("WARNING", color="<white><bold><bg yellow>")
+_loguru.level("ERROR", color="<white><bold><bg red>")
+_loguru.level("CRITICAL", color="<white><bold><bg red>")
+_loguru.level("SUCCESS", color="<black><bold><bg green>")
 
 class Logger:
-  def __init__(self):
-    self.logger = logger
-    self._console = None
-    self._file = None
-    self._log_to_console()
+    def __init__(self):
+        self.logger = _loguru
+        self._console = None
+        self._file = None
+        self._log_to_console()
 
-  def _log_to_console(self):
-    if self._console is None:
-      rich_handler = RichHandler(
-        rich_tracebacks=True, markup=True, log_time_format="%H:%M:%S", show_path=False
-      )
-      self._console = self.logger.add(
-        rich_handler,
-        format="{message}",
-        colorize=True,
-      )
+    def _log_to_console(self):
+        if self._console is None:
+            rich_handler = RichHandler(
+                rich_tracebacks=True, markup=True, log_time_format="%H:%M:%S", show_path=False
+            )
+            self._console = self.logger.add(rich_handler, format="{message}", colorize=True)
 
-  def _unlog_from_console(self):
-    if self._console is not None:
-      try:
-        self.logger.remove(self._console)
-      except Exception:
-        pass
-      self._console = None
+    def _unlog_from_console(self):
+        if self._console is not None:
+            try:
+                self.logger.remove(self._console)
+            except Exception:
+                pass
+            self._console = None
 
-  def set_verbosity(self, verbose):
-    if verbose:
-      self._log_to_console()
-    else:
-      self._unlog_from_console()
+    def set_verbosity(self, verbose):
+        if verbose:
+            self._log_to_console()
+        else:
+            self._unlog_from_console()
 
-  def debug(self, text):
-    self.logger.debug(text)
-
-  def info(self, text):
-    self.logger.info(text)
-
-  def warning(self, text):
-    self.logger.warning(text)
-
-  def error(self, text):
-    self.logger.error(text)
-
-  def critical(self, text):
-    self.logger.critical(text)
-
-  def success(self, text):
-    self.logger.success(text)
-
+    def debug(self, text): self.logger.debug(text)
+    def info(self, text): self.logger.info(text)
+    def warning(self, text): self.logger.warning(text)
+    def error(self, text): self.logger.error(text)
+    def critical(self, text): self.logger.critical(text)
+    def success(self, text): self.logger.success(text)
 
 logger = Logger()
