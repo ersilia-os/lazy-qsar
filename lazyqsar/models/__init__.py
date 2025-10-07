1a7f1c6dadcd9585a043dd3cb18bb00b13cd73a3
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2025-10-07 12:25:18 +0200

    cleaning up

--- lazyqsar/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .logistic_regression_binary_classifier import (
-    LazyLogisticRegressionBinaryClassifier,
-)
-from .random_forest_binary_classifier import LazyRandomForestBinaryClassifier
-
-try:
-    from .tune_tables_binary_classifier import LazyTuneTablesBinaryClassifier
-except ImportError:
-    LazyTuneTablesBinaryClassifier = None
