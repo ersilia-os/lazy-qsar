38ae1a27d3ef7c186cf417a64144481048cfa897
GemmaTuron <gemma@ersilia.io>
2025-10-08 16:32:18 +0200

    tests wip

--- tests/test_binary_classification.py
+++ tests/test_binary_classification.py
@@ -2,13 +2,14 @@ import csv
 import shutil
 
 from lazyqsar.qsar import LazyBinaryQSAR
+from lazyqsar.agnostic import LazyBinaryClassifier
 from sklearn.metrics import roc_auc_score
 
 from lazyqsar.utils.logging import logger
 
 
 def load_dataset(dataset_name):
-    with open("benchmark/data/{0}_train.csv".format(dataset_name), "r") as f:
+    with open("../benchmark/data/{0}_train.csv".format(dataset_name), "r") as f:
         reader = csv.reader(f)
         smiles_train = []
         y_train = []
@@ -18,7 +19,7 @@ def load_dataset(dataset_name):
             y_train += [int(row[1])]
     logger.info("Number of training samples: {0}".format(len(y_train)))
 
-    with open("benchmark/data/{0}_test.csv".format(dataset_name), "r") as f:
+    with open("../benchmark/data/{0}_test.csv".format(dataset_name), "r") as f:
         reader = csv.reader(f)
         smiles_test = []
         y_test = []
@@ -45,6 +46,26 @@ def fit_and_evaluate():
     )
     shutil.rmtree("test_binary_classification")
 
+def load_descriptors(dataset_name): #precalculated using Ersilia by each user individually
+    X_train = f"../benchmark/data/ersilia_preds/{dataset_name}_train.h5"
+    X_test = f"../benchmark/data/ersilia_preds/{dataset_name}_test.h5"
+    return X_train, X_test
+
+
+def fit_and_evaluate_agnostic():
+    model = LazyBinaryClassifier()
+    smiles_train, y_train, smiles_test, y_test = load_dataset("ames")
+    X_train, X_test = load_descriptors("ames")
+    model.fit(h5_file=X_train, y= y_train)
+    model.save("test_binary_agnostic_classification")
+    model = LazyBinaryClassifier.load("test_binary_agnostic_classification")
+    y_pred = model.predict_proba(h5_file = X_test)[:,1]
+    logger.info("ROC-AUC: {0}".format(roc_auc_score(y_test, y_pred)))
+    logger.info(
+        "Removing temporary files from {0}".format("test_binary_agnostic_classification")
+    )
+    shutil.rmtree("test_binary_agnostic_classification")
 
 if __name__ == "__main__":
-    fit_and_evaluate()
+    #fit_and_evaluate()
+    fit_and_evaluate_agnostic()
