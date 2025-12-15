439eb00882389b6bc0bf43a95eaceb3288fa3100
Abellegese <abellegese5@gmail.com>
2025-12-15 11:15:05 +0000

    adding missing module

---
+++ lazyqsar/utils/_install_extras.py
@@ -0,0 +1,97 @@
+import subprocess
+import sys
+import logging
+
+TORCH_VERSION = "2.8.0"
+RDKIT_VERSION = "2025.9.1"
+TORCH_CPU_EXTRA_INDEX_URL = "https://download.pytorch.org/whl/cpu"
+
+logger = logging.getLogger("installer")
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter("[%(levelname)s] %(message)s"))
+logger.addHandler(handler)
+
+
+def run_cmd(cmd):
+    process = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        bufsize=1,
+        universal_newlines=True,
+    )
+
+    for line in process.stdout:
+        logger.info(line.rstrip())
+
+    for line in process.stderr:
+        logger.error(line.rstrip())
+
+    process.wait()
+
+    if process.returncode != 0:
+        raise subprocess.CalledProcessError(process.returncode, cmd)
+
+
+def ensure_torch_cpu(version=TORCH_VERSION, index_url=TORCH_CPU_EXTRA_INDEX_URL):
+    try:
+        import torch  # noqa: F401
+        logger.info("Torch already installed; skipping installation.")
+        return
+    except ImportError:
+        logger.warning("Torch not found, installing CPU version...")
+    
+    cmd = [
+        sys.executable,
+        "-m",
+        "pip",
+        "install",
+        f"torch=={version}",
+        "--extra-index-url",
+        index_url,
+    ]
+
+    run_cmd(cmd)
+    logger.info("Torch CPU installation complete.")
+
+
+def ensure_chemprop():
+    try:
+        import chemprop  # noqa: F401
+        logger.info("Chemprop already installed; skipping installation.")
+        return
+    except ImportError:
+        logger.warning("Chemprop not found, installing...")
+
+    cmd = [
+        sys.executable,
+        "-m",
+        "pip",
+        "install",
+        "chemprop",
+    ]
+
+    run_cmd(cmd)
+    logger.info("Chemprop installation complete.")
+
+
+
+def ensure_rdkit(version=RDKIT_VERSION):
+    try:
+        import rdkit  # noqa: F401
+        logger.info("rdkit already installed; Upg.")
+    except:
+        pass
+    
+    cmd = [
+        sys.executable,
+        "-m",
+        "pip",
+        "install",
+        f"rdkit=={version}"
+    ]
+
+    run_cmd(cmd)
+    logger.info("Rdkit installation complete.")
